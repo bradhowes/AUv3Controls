60f3620452deebb3b2cc75c9a7e89c8e770e9478
Brad Howes <bradhowes@mac.com>
2024-08-17 21:06:54 +0200

    Investigate paths for snapshot images

--- Tests/AUv3ControlsTests/XCTest+Extensions.swift
+++ Tests/AUv3ControlsTests/XCTest+Extensions.swift
@@ -14,20 +14,31 @@ extension XCTest {
   }
 
   @inlinable
-  func assertSnapshot<V: SwiftUI.View>(matching: V, size: CGSize = CGSize(width: 220, height: 220),
-                                           file: StaticString = #file, testName: String = #function,
-                                           line: UInt = #line) throws {
+  func assertSnapshot<V: SwiftUI.View>(
+    matching: V,
+    size: CGSize = CGSize(width: 220, height: 220),
+    file: StaticString = #file, testName: String = #function,
+    line: UInt = #line
+  ) throws {
     isRecording = false
-    print(ProcessInfo.processInfo.environment)
 
+    let uniqueTestName = makeUniqueSnapshotName(testName)
     let isOnGithub = ProcessInfo.processInfo.environment["XCTestBundlePath"]?.contains("/Users/runner/work") ?? false
 
 #if os(iOS)
-    if let result = SnapshotTesting.verifySnapshot(of: matching,
-                                                as: .image(drawHierarchyInKeyWindow: false,
-                                                           layout: .fixed(width: size.width, height: size.height)),
-                                                named: makeUniqueSnapshotName(testName),
-                                                   file: file, testName: testName, line: line) {
+    if let result = SnapshotTesting.verifySnapshot(
+      of: matching,
+      as: .image(
+        drawHierarchyInKeyWindow: false,
+        layout: .fixed(width: size.width, height: size.height)
+      ),
+      named: uniqueTestName,
+      file: file,
+      testName: testName,
+      line: line
+    ) {
+      print("uniqueTestName:", uniqueTestName)
+      print("file:", file)
       if isOnGithub {
         print("***", result)
       } else {
