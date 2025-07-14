ec55efcfd1bc4f5acd8226f93ac30e5417321f38
Brad Howes <bradhowes@mac.com>
2025-07-14 12:29:19 +0200

    Define trait to enable use of CustomAlert package

--- Package.swift
+++ Package.swift
@@ -1,25 +1,14 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.1
 
 import PackageDescription
 
 // Set to `true` to depend on CustomAlert package
-let supportCustomAlert = false
-
-let packageDependencies: [Package.Dependency] = [
-  .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.20.0"),
-  .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.18.0"),
-  .package(url: "https://github.com/apple/swift-async-algorithms", from: "1.0.4"),
-  .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
-] + (supportCustomAlert ? [.package(url: "https://github.com/divadretlaw/CustomAlert.git", from: "4.1.0")] : [])
-
-let targetDependencies: [Target.Dependency] = [
-  .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
-  .product(name: "AsyncAlgorithms", package: "swift-async-algorithms"),
-] + (supportCustomAlert ? [.product(name: "CustomAlert", package: "CustomAlert")] : [])
+let useCustomAlert: Trait = .init(name: "useCustomAlert", description: "Use CustomAlert package")
 
 let swiftSettings: [SwiftSetting] = [
   .swiftLanguageMode(.v6),
-] + (supportCustomAlert ? [.define("SUPPORT_CUSTOM_ALERT")] : [])
+  .define("SUPPORT_CUSTOM_ALERT", .when(traits: [useCustomAlert.name]))
+]
 
 let package = Package(
   name: "AUv3Controls",
@@ -32,23 +21,42 @@ let package = Package(
       name: "AUv3Controls",
       targets: ["AUv3Controls"])
   ],
-  dependencies: packageDependencies,
+  traits: [useCustomAlert],
+  dependencies: [
+    .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.20.0"),
+    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.18.0"),
+    .package(url: "https://github.com/apple/swift-async-algorithms", from: "1.0.4"),
+    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+    .package(url: "https://github.com/divadretlaw/CustomAlert.git", from: "4.1.0", traits: [
+      .init(name: useCustomAlert.name)
+    ])
+  ],
   targets: [
     .target(
       name: "AUv3Controls",
-      dependencies: targetDependencies,
+      dependencies: [
+        .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+        .product(name: "AsyncAlgorithms", package: "swift-async-algorithms"),
+        .product(name: "CustomAlert", package: "CustomAlert", condition: .when(traits: [useCustomAlert.name]))
+      ],
       exclude: ["Examples/README.md", "Knob/README.md", "Toggle/README.md"],
       resources: [.process("Resources/Assets.xcassets")],
-      swiftSettings: swiftSettings
+      swiftSettings: [
+        .swiftLanguageMode(.v6),
+        .define("SUPPORT_CUSTOM_ALERT", .when(traits: [useCustomAlert.name]))
+      ]
     ),
     .testTarget(
       name: "AUv3ControlsTests",
-      dependencies: targetDependencies + [
+      dependencies: [
         "AUv3Controls",
         .product(name: "SnapshotTesting", package: "swift-snapshot-testing")
       ],
       exclude: ["__Snapshots__/"],
-      swiftSettings: swiftSettings
-   )
+      swiftSettings: [
+        .swiftLanguageMode(.v6),
+        .define("SUPPORT_CUSTOM_ALERT", .when(traits: [useCustomAlert.name]))
+      ]
+    )
   ]
 )
